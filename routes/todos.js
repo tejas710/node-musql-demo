0d5610f3309392ff3120e6edc7297a441ed4a14f
tejas710 <tejasvora710@gmail.com>
2016-05-19 19:10:17 +0530

    update nd delete complete

--- routes/todos.js
+++ routes/todos.js
@@ -6,6 +6,33 @@ router.get('/', function(req, res, next) {
   res.json({"message ": "it's working"});
 });
 
+// www.abc.com/todos/getall
+router.get('/getall', function(req,res,next){
+    models.Student.findAll({})
+    .then(function(stu){
+      res.json(stu);
+    }).error(function(error){
+      res.json(error);
+    })
+
+});
+//www.abc.com/todos/update
+router.post('/update', function(req,res,next){
+
+  models.Student.update({
+  firstname: 'Vivek',
+  lastname: 'shah'
+}, {where: { id: 2 } })
+.then(function(){
+  console.log(arguments) // { '0': [ 0 ] }
+});
+});
+
+router.post('/delete', function(req,res,next){
+  models.Student.destroy({where: {id:1}});
+
+});
+// www.abc.com/todos/add
 router.post('/add', function(req,res,next){
 var newstudent = models.Student.build(req.body);
 
